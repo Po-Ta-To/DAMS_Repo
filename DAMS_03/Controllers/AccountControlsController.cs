c6030afcda2e3a0a7beede0b971e1a2440986693
HTETTHIRIZAW\nimda <htetthirizawkaykay@gmail.com>
2017-07-07 12:54:36 +0800

    Added Index Views for Account & Listing Controls

---
+++ DAMS_03/Controllers/AccountControlsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAMS_03.Controllers
+{
+    public class AccountControlsController : Controller
+    {
+        // GET: AccountControls
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
