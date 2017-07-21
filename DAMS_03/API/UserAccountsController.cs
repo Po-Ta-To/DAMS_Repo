6496d67035d192e6afcf5c3b1e8b326778c1bc02
Gareth Lim <Gareth Lim@LAPTOP-N2HRHPII>
2017-07-21 16:24:19 +0800

    Changed KK's accounts

--- DAMS_03/API/UserAccountsController.cs
+++ DAMS_03/API/UserAccountsController.cs
@@ -9,6 +9,10 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using DAMS_03.Models;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNet.Identity;
 
 namespace DAMS_03.API
 {
@@ -70,20 +74,20 @@ namespace DAMS_03.API
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/UserAccounts
-        [ResponseType(typeof(UserAccount))]
-        public IHttpActionResult PostUserAccount(UserAccount userAccount)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// POST: api/UserAccounts
+        //[ResponseType(typeof(UserAccount))]
+        //public IHttpActionResult PostUserAccount(UserAccount userAccount)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.UserAccounts.Add(userAccount);
-            db.SaveChanges();
+        //    db.UserAccounts.Add(userAccount);
+        //    db.SaveChanges();
 
-            return CreatedAtRoute("DefaultApi", new { id = userAccount.ID }, userAccount);
-        }
+        //    return CreatedAtRoute("DefaultApi", new { id = userAccount.ID }, userAccount);
+        //}
 
         // DELETE: api/UserAccounts/5
         [ResponseType(typeof(UserAccount))]
@@ -114,5 +118,70 @@ namespace DAMS_03.API
         {
             return db.UserAccounts.Count(e => e.ID == id) > 0;
         }
+
+
+        // POST: api/UserAccounts
+        [ResponseType(typeof(UserAccount))]
+        public IHttpActionResult PostingUserAccount(UserAccount model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+
+            //var user = new ApplicationUser { UserName = userAccount.UserName, Email = userAccount.Email };
+
+
+
+            string aspID = (from AspNetUsers in db.AspNetUsers
+                            where AspNetUsers.UserName == model.AspNetUser.UserName
+                            select AspNetUsers.Id).First().ToString();
+
+            //string aspID = (from AspNetUsers in db.AspNetUsers
+            //                where AspNetUsers.Id == userAccount.AspNetID
+            //                select AspNetUsers.Id).First().ToString();
+
+            //string aspID = (from AspNetUsers in db.AspNetUsers
+            //                where userAccount.AspNetID == AspNetUsers.Id
+            //                select AspNetUsers.Id).First().ToString();
+
+            //string aspID = (from UserAccounts in db.UserAccounts
+            //                where userAccount.AspNetID == userAccount.AspNetUser.Id
+            //                select userAccount.AspNetUser.Id).First().ToString();
+
+            //string aspID = (from AspNetUsers in db.AspNetUsers
+            //                where model.AspNetID == AspNetUsers.Id
+            //                select model.AspNetID).First().ToString();
+
+            //string aspID = (from AspNetUsers in db.AspNetUsers
+            //                where AspNetUsers.UserName == model.UserName
+            //                select AspNetUsers.Id).First().ToString();
+
+            UserAccount addUserAccount = new UserAccount();
+
+                addUserAccount.ID = 1;
+                addUserAccount.NRIC = model.NRIC;
+                addUserAccount.Name = model.Name;
+                addUserAccount.DOB = model.DOB;
+                addUserAccount.Gender = model.Gender;
+                addUserAccount.Mobile = model.Mobile.ToString();
+                addUserAccount.Addrress = model.Addrress;
+                addUserAccount.AspNetID = aspID;
+
+
+                db.UserAccounts.Add(addUserAccount);
+                db.SaveChanges();
+                return CreatedAtRoute("PostApi", new { id = addUserAccount.AspNetUser.Id }, model);
+
+
+            }
+
+        
+        
+
+
+        
     }
 }
