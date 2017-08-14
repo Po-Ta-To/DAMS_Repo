8924afd3385b825f681d30f85eb75a7d57ec76b4
Sabrina <SABRINA.15@ichat.sp.edu.sg>
2017-08-14 10:59:11 +0800

    nav drawer bug

--- Dental-IT/Dental_IT.Android/Main/Select_Hospital.cs
+++ Dental-IT/Dental_IT.Android/Main/Select_Hospital.cs
@@ -10,6 +10,8 @@ using Android.Support.V7.App;
 using System;
 using System.Threading.Tasks;
 using Dental_IT.Model;
+using Android.Support.V4.Widget;
+using Android.Support.Design.Widget;
 
 namespace Dental_IT.Droid.Main
 {
@@ -25,6 +27,10 @@ namespace Dental_IT.Droid.Main
         private SearchView searchView;
 
         API api = new API();
+
+        private DrawerLayout drawerLayout;
+        private NavigationView navigationView;
+        private Intent intent;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -117,9 +123,60 @@ namespace Dental_IT.Droid.Main
                 toolbar.SetTitle(Resource.String.selectHosp_title);
                 SetSupportActionBar(toolbar);
 
-                //Set backarrow as Default
+                //Set navigation drawer
+                SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
                 SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+
+                drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+                navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+                navigationView.InflateHeaderView(Resource.Layout.sublayout_Drawer_Header);
+                navigationView.InflateMenu(Resource.Menu.nav_menu);
+                navigationView.SetCheckedItem(Resource.Id.nav_RequestAppt);
+            
             });
+
+
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                switch (e.MenuItem.ItemId)
+                {
+                    case Resource.Id.nav_Home:
+                        intent = new Intent(this, typeof(Main_Menu));
+                        StartActivity(intent);
+                        break;
+
+                    case Resource.Id.nav_RequestAppt:
+                        intent = new Intent(this, typeof(Select_Hospital));
+                        StartActivity(intent);
+                        break;
+
+                    case Resource.Id.nav_MyAppt:
+                        intent = new Intent(this, typeof(My_Appointments));
+                        StartActivity(intent);
+                        break;
+
+                    case Resource.Id.nav_TreatmentInfo:
+                        intent = new Intent(this, typeof(Treatment_Information));
+                        StartActivity(intent);
+                        break;
+
+                    case Resource.Id.nav_Search:
+                        intent = new Intent(this, typeof(Search));
+                        StartActivity(intent);
+                        break;
+
+                    case Resource.Id.nav_ClearData:
+                        ClearData();
+                        break;
+
+                    case Resource.Id.nav_Logout:
+                        Logout();
+                        break;
+                }
+
+                //  React to click here and swap fragments or navigate
+                drawerLayout.CloseDrawers();
+            };
         }
 
         //  Implement menus in the action bar; backarrow
@@ -177,6 +234,66 @@ namespace Dental_IT.Droid.Main
             }
 
             return filteredList;
+        }
+
+        //  Logout function
+        public void Logout()
+        {
+            //  Logout confirmation dialog
+            Android.App.AlertDialog.Builder logoutConfirm = new Android.App.AlertDialog.Builder(this);
+            logoutConfirm.SetMessage(Resource.String.logout_text);
+            logoutConfirm.SetNegativeButton(Resource.String.confirm_logout, delegate
+            {
+                //  Remove user session from shared preferences
+                ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+                ISharedPreferencesEditor editor = prefs.Edit();
+                editor.Remove("remembered");
+                editor.Apply();
+
+                //  Clear user data
+                UserAccount.AccessToken = null;
+                UserAccount.Name = null;
+                UserAccount.ID = 0;
+
+                //  Redirect to sign in page
+                intent = new Intent(this, typeof(Sign_In));
+                StartActivity(intent);
+            });
+            logoutConfirm.SetNeutralButton(Resource.String.cancel, delegate
+            {
+                logoutConfirm.Dispose();
+            });
+            logoutConfirm.Show();
+        }
+
+
+        //  Clear data function
+        public void ClearData()
+        {
+            //  Logout confirmation dialog
+            Android.App.AlertDialog.Builder clearConfirm = new Android.App.AlertDialog.Builder(this);
+            clearConfirm.SetMessage(Resource.String.clearData_text);
+            clearConfirm.SetNegativeButton(Resource.String.confirm_clearData, delegate
+            {
+                //  Remove user data from shared preferences
+                ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+                ISharedPreferencesEditor editor = prefs.Edit();
+                editor.Clear();
+                editor.Apply();
+
+                //  Clear remaining user data
+                UserAccount.AccessToken = null;
+                UserAccount.Name = null;
+
+                //  Redirect to sign in page
+                intent = new Intent(this, typeof(Sign_In));
+                StartActivity(intent);
+            });
+            clearConfirm.SetNeutralButton(Resource.String.cancel, delegate
+            {
+                clearConfirm.Dispose();
+            });
+            clearConfirm.Show();
         }
     }
 }
